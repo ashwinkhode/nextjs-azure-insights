98da5b83eaa4e735e7201b104d655e6b203de41a
Ashwin Khode <ashwin.khode1@gmail.com>
2021-08-20 16:20:57 +0530

    add: analytics for app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import {
+	AppInsightsContext,
+	AppInsightsErrorBoundary,
+} from "@microsoft/applicationinsights-react-js";
+import { reactPlugin } from "../utils/AppInsights";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<AppInsightsErrorBoundary
+			onError={() => (
+				<h1>I believe something went wrong. Please Refresh the page!</h1>
+			)}
+			appInsights={reactPlugin}
+		>
+			<AppInsightsContext.Provider value={reactPlugin}>
+				<Component {...pageProps} />
+			</AppInsightsContext.Provider>
+		</AppInsightsErrorBoundary>
+	);
 }
-export default MyApp
+export default MyApp;
